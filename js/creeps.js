9900c5a5c5f0608db07fa5d55803ecb41999d782
Kyle Cooper <kylegcooper@gmail.com>
2018-04-04 21:23:29 -0600

    first round creep logic

---
+++ js/creeps.js
@@ -0,0 +1,69 @@
+class Creep {
+    move(elapsedTime){
+        if (direction == 'up'){
+            this.y -= speed*elapsedTime/1000;
+        }
+        else if (direction == 'down'){
+            this.y += speed*elapsedTime/1000;
+        }
+        else if (direction == 'left'){
+            this.x -= speed*elapsedTime/1000;
+        }
+        else if (direction == 'right'){
+            this.x += speed.elapsedTime/1000;
+        }
+    }
+}
+
+class groundOne extends Creep {
+    constructor(x, y, direction){
+        this.hp = 50;
+        this.speed = 10;
+        this.flying = false;
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+}
+
+
+
+class groundTwo extends Creep {
+    constructor(x,y,direction){
+        this.hp = 75;
+        this.speed = 12;
+        this.flying = false;
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+}
+
+class groundThree extends Creep {
+    constructor(x,y,direction){
+        this.hp = 100;
+        this.speed = 10;
+        this.flying = false;
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+}
+
+class air extends Creep {
+    constructor(x,y,direction){
+        this.hp = 50;
+        this.speed = 10;
+        this.flying = true;
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+}    
+
+let jim = groundOne(1,1,'right');
+jim.move(1000);
+console.log(jim);
+jim.direction = 'down';
+jim.move(2000);
+console.log(jim);
